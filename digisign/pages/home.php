02eb5ee2a84057316728ebb1c688434f47ab3d42
AlaaSall <salsabilamobile636@gmail.com>
2026-05-29 01:47:48 +0700

    Delete digisign directory

--- digisign/pages/home.php
+++
@@ -1,4 +0,0 @@
-<div class="max-w-5xl mx-auto">
-    <h1 class="text-2xl font-bold mb-4">Dashboard</h1>
-    <p class="text-slate-600">Selamat datang kembali, <?= htmlspecialchars($_SESSION['username']) ?></p>
-</div>
